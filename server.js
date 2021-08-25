e5f841da09d37cf4930855e57cd515a1c974df35
charlie roberts <charlie@charlie-roberts.com>
2021-08-25 09:31:37 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,25 @@
+const http = require('http'),
+      fs   = require('fs'),
+      port = 3000
+
+const server = http.createServer( function( request,response ) {
+  switch( request.url ) {
+    case '/':
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
+      break
+    default:
+      response.end( '404 Error: File Not Found' )
+  }
+})
+
+server.listen( process.env.PORT || port )
+
+const sendFile = function( response, filename ) {
+   fs.readFile( filename, function( err, content ) {
+     file = content
+     response.end( content, 'utf-8' )
+   })
+}
