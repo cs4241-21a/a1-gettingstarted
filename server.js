52915838d7ba1af4c5326fb1653d3f6071363e40
Ryan Stebe <rjstebe1@gmail.com>
2021-08-26 16:32:32 -0400

    changed server to find any file correctly

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
+const { send } = require('process')
+
 const http = require('http'),
       fs   = require('fs'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
+  if (request.url[request.url.length - 1] == '/'){
+    sendFile(response, request.url + 'index.html');
+  }
+  else{
+    sendFile(response, request.url);
   }
 })
 
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
+  fs.readFile( filename, function( err, content ) {
+    if (err) {
+      response.end( '404 Error: File Not Found' );
+    }
+    else{
+      response.end( content, 'utf-8' )
+    }
+  })
 }
