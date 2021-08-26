2a57f5d9409a05e941832e5359ca981a9ce5471a
Jack Sullivan <jhaldisul@gmail.com>
2021-08-26 16:17:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,24 @@
-const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+const http = require("http"),
+  fs = require("fs"),
+  port = 3000;
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
+const server = http.createServer(function (request, response) {
+  switch (request.url) {
+    case "/":
+      sendFile(response, "index.html");
+      break;
+    case "/index.html":
+      sendFile(response, "index.html");
+      break;
     default:
-      response.end( '404 Error: File Not Found' )
+      response.end("404 Error: File Not Found");
   }
-})
+});
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port);
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
-}
+const sendFile = function (response, filename) {
+  fs.readFile(filename, function (err, content) {
+    response.end(content, "utf-8");
+  });
+};
