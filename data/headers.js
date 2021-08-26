17da931fbc294477f7504561f7e1c5d4cbad8124
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-08-25 20:26:34 -0400

    Build core website with Mustache.js

---
+++ data/headers.js
@@ -0,0 +1,12 @@
+const headers = {
+    readme: {
+        path: 'C:\\Users\\Nathan\\README.md',
+        title: 'Hello, I\'m Nathan Klingensmith',
+        subtitle: 'I am a UI/UX designer, full stack developer, and team leader.'
+    },
+    education: {
+        path: 'C:\\Users\\Nathan\\Education.zip',
+        title: 'Education',
+        subtitle: 'I am pursuing a BS in <strong>Computer Science</strong> and minor in <strong>Interactive Media & Game Development</strong> as part of WPI\'s class of 2022.'
+    }
+}
---
+++ data/roles.js
@@ -0,0 +1,17 @@
+const roles = [
+  {
+    title: 'Head of Technology',
+    subtitle: 'Pallyt Technologies',
+    tag: '2021'
+  },
+  {
+    title: 'Angular Web Developer',
+    subtitle: 'Bio-CS Bridge Project',
+    tag: '2019 - 2021'
+  },
+  {
+    title: 'Network Automation Intern',
+    subtitle: 'State Street Corporation',
+    tag: '2019'
+  }
+];
