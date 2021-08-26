6b4046d8ccb72692cc2426c5c5e55d1049f69bb4
Nathan Klingensmith <njklingensmith@wpi.edu>
2021-08-25 20:53:13 -0400

    Update package.json, add Skills section

--- data/headers.js
+++ data/headers.js
@@ -8,5 +8,10 @@ const headers = {
         path: 'C:\\Users\\Nathan\\Education.zip',
         title: 'Education',
         subtitle: 'I am pursuing a BS in <strong>Computer Science</strong> and minor in <strong>Interactive Media & Game Development</strong> as part of WPI\'s class of 2022.'
+    },
+    skills: {
+        path: 'C:\\Users\\Nathan\\Skills.zip',
+        title: 'Skills',
+        subtitle: 'From my experience, both professional and academic, I have accumulated a number of skills in both the design and development space.'
     }
 }
