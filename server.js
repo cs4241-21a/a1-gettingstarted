5fdd16fa4a842116792a4e688f0e222b9b74e4b1
Evdude33 <evanllewellyn33@gmail.com>
2021-08-28 18:42:27 -0400

    Initial Changes

--- server.js
+++ server.js
@@ -3,23 +3,28 @@ const http = require('http'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
+  sendFile( response, request.url)
+  // switch( request.url ) {
+  //   case '/':
+  //     sendFile( response, 'index.html' )
+  //     break
+  //   case '/index.html':
+  //     sendFile( response, 'index.html' )
+  //     break
+  //   default:
+  //     response.end( '404 Error: File Not Found' )
+  // }
 })
 
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
+  filename = filename.substring(1)
    fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
+     if(err) {
+        response.end('404 Error: File Not Found')
+     } else {
+        response.end( content, 'utf-8' )
+     }
    })
 }
