60b6ec2eeed0f907a498fcce41f59be48b8893b1
unknown <t.kgoonx@gmail.com>
2021-08-28 14:36:20 -0400

    Completed bare minimum req

--- server.js
+++ server.js
@@ -3,13 +3,18 @@ const http = require('http'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
+  console.log(request.url, request.url === '/index.css')
+  switch( request.url) {
     case '/':
       sendFile( response, 'index.html' )
       break
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/index.css':
+      console.log('Sending file')
+      sendFile(response, 'index.css');
+      break;
     default:
       response.end( '404 Error: File Not Found' )
   }
