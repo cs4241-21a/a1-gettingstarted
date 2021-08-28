fa66a691998590755504371641ab2c11c472a287
G3P <73323107+GP2P@users.noreply.github.com>
2021-08-28 19:45:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,25 +1,31 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+	fs = require('fs'),
+	port = 3000
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
+const server = http.createServer(function (request, response) {
+	switch (request.url) {
+		case '/':
+			sendFile(response, 'index.html')
+			break
+		case '/index.html':
+			sendFile(response, 'index.html')
+			break
+		case '/styles.css':
+			sendFile(response, 'styles.css')
+			break
+		case '/G3P-3D-REN-Filled-Fixed-Transparent.png':
+			sendFile(response, 'G3P-3D-REN-Filled-Fixed-Transparent.png')
+			break
+		default:
+			response.end('404 Error: File Not Found')
+	}
 })
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
+const sendFile = function (response, filename) {
+	fs.readFile(filename, function (err, content) {
+		file = content
+		response.end(content, 'utf-8')
+	})
 }
