3f3e58a6152a0da35a6e0e42588f4eb0248f2b11
emmaline-raven <ejraven@wpi.edu>
2021-08-29 14:50:16 -0400

    Updated server to load styles.css

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/styles.css':
+      sendFile( response, 'styles.css' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
