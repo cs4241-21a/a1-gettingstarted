248c50d6058d1893a81f30efb3b5a3a8151d76e2
Eli Benevedes <benevedeselias@gmail.com>
2021-08-29 18:23:04 -0400

    Add message board support

---
+++ client.js
@@ -0,0 +1,64 @@
+function getAndSetMessageTable()
+{
+	let httpReq = new XMLHttpRequest();
+	httpReq.open("GET", "/get_messages", false);
+	httpReq.send(null);
+
+	console.log(httpReq.responseText);
+
+	let data = JSON.parse(httpReq.responseText);
+
+	// Get the table and clear it
+	table = document.getElementById("message_table");
+	table.innerHTML = "";
+
+	// Newer messages should be higher up
+	data.messages.reverse();
+
+	// Create rest of table
+	data.messages.forEach(message => {
+		let row = table.insertRow(0);
+
+		let name_cell = row.insertCell(0);
+		let msg_cell = row.insertCell(1);
+		let date_cell = row.insertCell(2);
+
+		name_cell.innerHTML = message.name;
+		msg_cell.innerHTML = message.msg;
+		date_cell.innerHTML = message.date;
+	});
+
+	// Insert table headers
+	header_row = table.insertRow(0);
+
+	header_row.insertCell(0).innerHTML = "<h3>Name</h3>";
+	header_row.insertCell(1).innerHTML = "<h3>Message</h3>";
+	header_row.insertCell(2).innerHTML = "<h3>Date</h3>";
+
+	console.log("Updated messages table");
+}
+
+function postMessage()
+{
+	let name = document.getElementById("name").value;
+	let msg  = document.getElementById("msg").value;
+	let date = Date().toString();
+
+	let data = {
+		"name": name,
+		"msg": msg,
+		"date": date
+	};
+
+	let httpReq = new XMLHttpRequest();
+	httpReq.open("POST", "/send_message", false);
+	
+	httpReq.setRequestHeader("Content-Type", "application/json");
+	httpReq.send(JSON.stringify(data));
+
+	getAndSetMessageTable();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  getAndSetMessageTable();
+});
--- server.js
+++ server.js
@@ -3,23 +3,124 @@ const http = require('http'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
+  if(request.method === "POST")
+  {
+    switch(request.url) {
+      case '/send_message':
+        let body = "";
+        request.on("data", (chunk) => {
+          body += chunk;
+        });
+
+        request.on("end", () => {
+          handleSendMessage(request, response, JSON.parse(body));
+        });
+
+        break;
+      
+        default:
+          console.log("Bad route: " + request.url);
+          response.end(400);
+          break;
+    }
   }
-})
+  else
+  {
+    console.log(request.url);
+    switch( request.url ) {
+      case '/':
+        sendFile( response, 'index.html' )
+        break;
+
+      case '/index.html':
+        sendFile( response, 'index.html' )
+        break;
+
+      case '/client.js':
+        sendFile(response, 'client.js');
+        break;
+
+      case '/get_messages':
+        handleGetMessages(request, response);
+        break;
+
+      default:
+        console.log("Failed to find file: " + request.url);
+        response.end( '404 Error: File Not Found' );
+    }
+  }
+});
 
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
+  fs.readFile( filename, function( err, content ) {
+    response.end( content, 'utf-8' );
+  });
+}
+
+const handleGetMessages = function(req, res)
+{
+  console.log("Getting messages");
+
+  let empty_messages = {
+    "messages": []
+  };
+
+  fs.readFile("messages.json", "utf8", (err, data) => {
+    if(err)
+    {
+      console.log(err);
+
+      res.end(JSON.stringify(empty_messages));
+      return;
+    }
+    
+    console.log("messages.json:");
+    console.log(data);
+
+    res.end(data);    
+  });
+}
+
+const handleSendMessage = function(req, res, body)
+{
+  let msg = null;
+
+  console.log("Message received");
+  console.log(body);
+
+  fs.readFile("messages.json", "utf8", (err, data) => {
+    if(err)
+    {
+      console.log(err);
+      res.end("Failed");
+      return;
+    }
+
+    messages = JSON.parse(data).messages;  
+    console.log(messages);  
+
+    messages.push({
+      "name": body.name,
+      "msg": body.msg,
+      "date": body.date
+    });
+
+    console.log(messages);
+
+    msg = {
+      "messages": messages
+    };
+
+    res.end("Success");
+
+    fs.writeFile("messages.json", JSON.stringify(msg), (err) =>
+    {
+      if (err) throw err;
+
+      console.log("Wrote new messages.json");
+      console.log(JSON.stringify(msg));
+    });
+  });
 }
