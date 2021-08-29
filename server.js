968fd6ae133b90b3fe39e67b30df4c85b4d8b858
hmkyriacou <hmkyriacou14@gmail.com>
2021-08-29 18:04:46 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,35 +3,25 @@ const http = require('http'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
-  sendFile( response, request.url )
-
-  //switch( request.url ) {
-  //  case '/':
-  //    sendFile( response, 'index.html' )
-  //    break
-  //  case '/index.html':
-  //    sendFile( response, 'index.html' )
-  //    break
-  //  default:
-  //    response.end( '404 Error: File Not Found' )
-  //}
+  switch( request.url ) {
+    case '/':
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
+      break
+    case '/mystyle.css':
+      sendFile(response, 'mystyle.css')
+      break
+    default:
+      response.end( '404 Error: File Not Found' )
+  }
 })
 
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
-    
-    filename = filename.substring(1)
-    
-    if (filename === ""){
-        filename = "index.html"
-    }
-
    fs.readFile( filename, function( err, content ) {
-     if (err) {
-        response.end( '404 Error: File not found' )
-     }else{
-        response.end(content, 'utf-8')
-     }
+     response.end( content, 'utf-8' )
    })
 }
