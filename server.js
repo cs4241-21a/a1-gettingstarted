fdae06fba36330dc1769e569cba389977e35f29d
MJ <matt.d.johannesen@gmail.com>
2021-08-29 15:50:56 -0400

    Initial commit

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const server = http.createServer( function( request,response ) {
     case '/index.html':
       sendFile( response, 'index.html' )
       break
+    case '/mainstyles.css':
+      sendFile( response, 'mainstyles.css' )
+      break
+    case '/cards.css':
+      sendFile( response, 'cards.css' )
+      break
     default:
       response.end( '404 Error: File Not Found' )
   }
