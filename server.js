19ed35a0ffba864b37c97dd44f0ab96345cdc3d5
Evdude33 <evanllewellyn33@gmail.com>
2021-08-29 16:13:31 -0400

    Fixing server issue

--- server.js
+++ server.js
@@ -3,13 +3,21 @@ const http = require('http'),
       port = 3000
 
 const server = http.createServer( function( request,response ) {
-  sendFile( response, request.url)
+  switch( request.url ) {
+    case '/':
+      sendFile( response, 'index.html' )
+      break
+    case '/index.html':
+      sendFile( response, 'index.html' )
+      break
+    default:
+      response.end( '404 Error: File Not Found' )
+  }
 })
 
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
-  filename = filename.substring(1)
    fs.readFile( filename, function( err, content ) {
      if(err) {
         response.end('404 Error: File Not Found')
