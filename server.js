7b3d8b6775d80bd2c0267f870a25dc618b35c8c9
aburke921 <aburke@wpi.edu>
2021-08-30 08:33:33 -0400

    Fixed bug in server.js
    According to the prof, the line `file = content` is a bug and should be removed.

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
    fs.readFile( filename, function( err, content ) {
-     file = content
      response.end( content, 'utf-8' )
    })
 }
