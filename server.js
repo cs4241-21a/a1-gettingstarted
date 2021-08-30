11e6a709c3b8d9b4be474a38bc5517795354ad8a
Jasper Meggitt <jasper.meggitt@gmail.com>
2021-08-30 11:25:52 -0400

    Fill out index.html

--- server.js
+++ server.js
@@ -1,25 +1,28 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+    fs = require('fs'),
+    port = 3000
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
-      sendFile( response, 'index.html' )
-      break
-    default:
-      response.end( '404 Error: File Not Found' )
-  }
+const server = http.createServer(function (request, response) {
+    switch (request.url) {
+        case '/':
+            sendFile(response, 'index.html')
+            break
+        case '/index.html':
+            sendFile(response, 'index.html')
+            break
+        case '/index.css':
+            sendFile(response, 'index.css')
+            break
+        default:
+            response.end('404 Error: File Not Found')
+    }
 })
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     file = content
-     response.end( content, 'utf-8' )
-   })
+const sendFile = function (response, filename) {
+    fs.readFile(filename, function (err, content) {
+        file = content
+        response.end(content, 'utf-8')
+    })
 }
