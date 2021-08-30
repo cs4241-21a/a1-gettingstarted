fca63f6820ccf92c503a0522e5d21de3808909af
gdgarsson <65737765+gdgarsson@users.noreply.github.com>
2021-08-30 11:10:53 -0400

    Fixed fade-in
    Image fade in "fixed", but the 2nd image has been removed as it would still not fade in. Big TY to Akim.

--- scrolltext.js
+++ scrolltext.js
@@ -4,37 +4,22 @@ var pageHeight;
 var pageHeightMax;
 
 var fp1;
-var DSCSpace;
-
-var fadeFP1In = false;
-var fadeDSCIn = false;
 
 var scrolling;
 var fp1fading;
-var dscfading;
+var opacity = 0, dopacity = 0;
 
 window.onload = function() {
     body = document.body;
     html = document.documentElement;
     fp1 = document.getElementById("fp1");
-    DSCSpace = document.getElementById("DSCSpace");
 
     fp1.style.opacity = 0;
-    DSCSpace.style.opacity = 0;
 
     pageHeight = window.innerHeight;
     pageHeightMax = pageHeight;
     body.style.paddingTop = pageHeight + "px";
-    //console.log(pageHeight);
-    
     scrolling = setInterval(scrollUp, 1);
-    /*if(fp1FadeIn){
-        setInterval(fp1FadeIn, 1);
-    }
-    if(DSCFadeIn){
-        setInterval(DSCFadeIn, 1);
-    }*/
-    //startFP1FadeIn();
 }
 
 function scrollUp(){
@@ -46,44 +31,18 @@ function scrollUp(){
     }
     else {
         clearInterval(scrolling);
-        fp1FadeIn = true;
-        startFP1FadeIn();
+        fp1fading = setInterval(fplFadeIn, 300);
     }
     body.style.paddingTop = pageHeight + "px";
 }
 
-function startFP1FadeIn(){
-    fp1fading = setInterval(fp1FadeIn, 1);
-}
-
-function fp1FadeIn(){
-    console.log("hi");
-    if(fp1.style.opacity > 1){
-        fp1.style.opacity = 1;
-    }
-    else if (fp1.style.opacity < 1){
-        fp1.style.opacity += 0.001;
+function fplFadeIn(){
+    if(opacity <= 100){
+		opacity += 0.1
+        fp1.style.opacity = opacity;
+		console.log(opacity);
     }
     else {
-        
         clearInterval(fp1fading);
-        DSCFadeIn = true;
-        dscfading = setInterval(DSCFadeIn, 1);
-    }
-}
-
-function startDSCFadeIn(){
-    fp1fading = setInterval(DSCFadeIn, 1);
-}
-
-function DSCFadeIn(){
-    if(DSCSpace.style.opacity > 1){
-        DSCSpace.style.opacity = 1;
-    }
-    else if (DSCSpace.style.opacity < 1){
-        DSCSpace.style.opacity += 0.001;
-    }
-    else {
-        clearInterval(dscfading);
     }
 }
